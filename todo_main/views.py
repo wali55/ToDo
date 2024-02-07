7f6ff736d0056eacba50d5e88e80aad756f9890a
wali55 <walisantunu@gmail.com>
2024-02-07 22:28:12 +0600

    addTask

--- todo_main/views.py
+++ todo_main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from todo.models import Task
 
 def home(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(is_completed=False)
     context = {
         'tasks': tasks,
     }
